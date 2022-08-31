f108f89be627527848fead4d8ed93029dd1ff4ba
raghu.s <raghu.s@menerva.tech>
2022-08-31 23:35:57 +0530

    Added frontend code

---
+++ Frontend/Features/Styles.js
@@ -0,0 +1,41 @@
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+
+export default {
+  Point: new Style({
+    image: new CircleStyle({
+      radius: 10,
+      fill: null,
+      stroke: new Stroke({
+        color: "magenta",
+      }),
+    }),
+  }),
+  Polygon: new Style({
+    stroke: new Stroke({
+      color: "gradient(to bottom, blue, white)",
+      lineDash: [4],
+      width: 3,
+    }),
+    fill: new Fill({
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
+  }),
+  MultiPolygon1: new Style({
+    stroke: new Stroke({
+      color: "gradient(to bottom, blue, white)",
+      width: 1,
+    }),
+    fill: new Fill({
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
+  }),
+  MultiPolygon2: new Style({
+    stroke: new Stroke({
+      color: "red",
+      width: 1,
+    }),
+    fill: new Fill({
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
+  }),
+};
